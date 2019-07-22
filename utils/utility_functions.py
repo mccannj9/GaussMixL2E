7fd885fdced7a4c43c840d8731424ed466bfb367
Jamie McCann <jamie.mccann@radarservices.com>
2019-07-22 18:33:18 +0200

    Committing vectorized, safe softmax, softplus, sigmoid functions

---
+++ utils/utility_functions.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+import numpy as np
+
+def softplus(x):
+    return np.log(1 + np.exp(-np.abs(x))) + np.maximum(x, 0)
+
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+if __name__ == "__main__":
+    x = np.random.randn(5)
+    print("Softmax:  ", softmax(x))
+    print("Softplus: ", softplus(x))
+    print("Sigmoid:  ", sigmoid(x))
