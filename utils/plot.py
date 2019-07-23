24c628cd964ff0e958c3a186acf006178b1d6697
Jamie McCann <jamie.mccann@radarservices.com>
2019-07-23 13:25:47 +0200

    Splitting utility functions into separate files based on function

---
+++ utils/plot.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+
+if __name__ == "__main__":
+    pass
---
+++ utils/simulate.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+
+def build_toy_dataset(N, K=None, D=None, pi=None):
+    pi = np.array([0.4, 0.6])
+    mus = [[2, 2], [-2, -2]]
+    stds = [[[1, 0.5], [0, 1]], [[1, 0], [0.5, 1]]]
+    x = np.zeros((N, 3), dtype=np.float32)
+    for n in range(N):
+        k = np.argmax(np.random.multinomial(1, pi))
+        x[n, 0:2] = np.random.multivariate_normal(mus[k], stds[k])
+        x[n, 2] = k
+    return x
+
+
+def build_toy_dataset_with_outliers(N, K=None, D=None, pi=None):
+    pi = np.array([0.375, 0.575, 0.05])
+    mus = [[1, 1], [-1, -1], [-5.5, -5.5]]
+    stds = [[0.1, 0.1], [0.1, 0.1], [0.1, 0.1]]
+    x = np.zeros((N, 3), dtype=np.float32)
+    for n in range(N):
+        k = np.argmax(np.random.multinomial(1, pi))
+        x[n, 0:2] = np.random.multivariate_normal(mus[k], np.diag(stds[k]))
+        x[n, 2] = k
+    return x
+
+if __name__ == "__main__":
+    pass
