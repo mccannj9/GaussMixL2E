b8e8e0970bfe7a0199d9102f6a48c0694c96bd7d
Jamie McCann <jamie.mccann@radarservices.com>
2019-07-24 12:27:59 +0200

    Added mahalanobis distance function for outlier detection

--- utils/math.py
+++ utils/math.py
@@ -15,6 +15,31 @@ def softmax(x):
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+
+def mahalanobis(data, mu, cov, k=2):
+    """
+    returns mahalanobis distance of each input for
+    each gaussian parameterized by mu[i], cov[i].
+
+    mu = (k, d) numpy array where k is number of mixtures
+    and d is the dimension of the data.
+
+    cov = (k, d, d) numpy array with the same setup as mu
+    in terms of k and d.
+
+    """
+    mhd = np.zeros((data.shape[0], k))
+
+    for i in range(k):
+        _mu = mu[i, :]
+        _cov = cov[i, :, :]
+
+        mhd[:, i] = np.sum(
+            (data - _mu) @ np.linalg.inv(_cov) * (data - _mu), axis=1
+        )
+
+    return np.sqrt(mhd)
+
 if __name__ == "__main__":
     x = np.random.randn(5)
     print("Softmax:  ", softmax(x))
